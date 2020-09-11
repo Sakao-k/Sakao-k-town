6ccff7f490de01d720ef5e29982505787945f80f
KumananNesa <kumanan.nesarajah@etu.u-pec.fr>
2020-09-12 01:47:05 +0200

    First commit

---
+++ src/sakao_server/ServerSakao.java
@@ -0,0 +1,41 @@
+package sakao_server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.json.JSONException;
+
+import sakao_connection_pool.DataSource;
+
+public class ServerSakao {
+	private ServerSocket serverSocket;
+	private static DataSource datasource;
+
+	public void start(int port) throws IOException, JSONException, ClassNotFoundException {
+		serverSocket = new ServerSocket(port);
+		datasource = new DataSource();
+		while (true) {
+			new ClientThread(serverSocket.accept()).start();
+		}
+	}
+
+	public void CloseConnection() throws IOException {
+		serverSocket.close();
+		System.out.println("Server deconnecte");
+	}
+
+	public ServerSocket getServerSocket() {
+		return serverSocket;
+	}
+
+	public void setServerSocket(ServerSocket serverSocket) {
+		this.serverSocket = serverSocket;
+	}
+
+	public static void main(String[] args) throws IOException, JSONException, ClassNotFoundException {
+		ServerSakao serveur1 = new ServerSakao();
+		serveur1.start(3030);
+		serveur1.CloseConnection();
+	}
+
+}
